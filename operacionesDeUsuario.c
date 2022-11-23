566745d0eafcffeb0372b84de911a239e200a6d4
NDondero <118403082+NDondero@users.noreply.github.com>
2022-11-23 16:18:01 -0300

    Levenshtein funcionando

--- operacionesDeUsuario.c
+++ operacionesDeUsuario.c
@@ -213,3 +213,95 @@ nodoA* palabraMayorFrecuencia(nodoA* motor)
     }
     return NULL;
 }
+/**La funcion del punto 5 busca en todo el arbol el nodo con mayor frecuencia y lo devuelve **/
+
+/// 6. Utilizar la distancia de levenshtein en el ingreso de una palabra
+///    y sugerir similares a partir de una distancia <= 3.
+int Minimo(int a, int b)
+{
+    if(a < b) return a;
+    return b;
+}
+
+int Levenshtein(char *s1,char *s2)
+{
+    int t1,t2,i,j,*m,costo,res,ancho;
+
+// Calcula tamanios strings
+    t1=strlen(s1);
+    t2=strlen(s2);
+
+// Verifica que exista algo que comparar
+    if (t1==0) return(t2);
+    if (t2==0) return(t1);
+    ancho=t1+1;
+
+// Reserva matriz con malloc                     m[i,j] = m[j*ancho+i] !!
+    m=(int*)malloc(sizeof(int)*(t1+1)*(t2+1));
+    if (m==NULL) return(-1); // ERROR!!
+
+// Rellena primera fila y primera columna
+    for (i=0; i<=t1; i++) m[i]=i;
+    for (j=0; j<=t2; j++) m[j*ancho]=j;
+
+// Recorremos resto de la matriz llenando pesos
+    for (i=1; i<=t1; i++) for (j=1; j<=t2; j++)
+        {
+            if (s1[i-1]==s2[j-1]) costo=0;
+            else costo=1;
+            m[j*ancho+i]=Minimo(Minimo(m[j*ancho+i-1]+1,     // Eliminacion
+                                       m[(j-1)*ancho+i]+1),              // Insercion
+                                m[(j-1)*ancho+i-1]+costo);
+        }      // Sustitucion
+
+// Devolvemos esquina final de la matriz
+    res=m[t2*ancho+t1];
+    free(m);
+    return(res);
+}
+
+nodoA* sugerirPalabra(nodoA* motor, char* palabra) // levenshtein <= 3
+{
+    nodoA* izqMenor, * derMenor;
+    int LevMot, LevIzq, LevDer;
+    if(motor)
+    {
+        izqMenor = sugerirPalabra(motor->izq, palabra);
+        derMenor = sugerirPalabra(motor->der, palabra);
+        LevMot = Levenshtein(motor->palabra, palabra);
+        if(izqMenor && derMenor)
+        {
+            LevIzq = Levenshtein(izqMenor->palabra, palabra);
+            LevDer = Levenshtein(derMenor->palabra, palabra);
+            if(LevMot <= LevIzq && LevMot <= LevDer)
+            {
+                return motor;
+            }
+            else if(LevIzq <= LevDer)
+            {
+                return izqMenor;
+            }
+            return derMenor;
+        }
+        else if(izqMenor)
+        {
+            LevIzq = Levenshtein(izqMenor->palabra, palabra);
+            if(LevIzq < LevMot)
+            {
+                return izqMenor;
+            }
+        }
+        else if(derMenor)
+        {
+            LevDer = Levenshtein(derMenor->palabra, palabra);
+            if(LevDer < LevMot)
+            {
+                return derMenor;
+            }
+        }
+        return motor;
+    }
+    return NULL;
+}
+/**La funcion del punto 6 devuelve el nodo que contiene la palabra con menor distancia
+y potencialmente la lexicograficamente mas cercana **/
--- operacionesDeUsuario.h
+++ operacionesDeUsuario.h
@@ -14,5 +14,7 @@ int ocurrenciaContigua(nodoT*, int, int);
 int fraseRelativaAOcurrencia(nodoA*, char[][20], int, int);
 int buscarFrase(nodoA*, char*, int, nodoT**);
 nodoA* palabraMayorFrecuencia(nodoA* motor);
+int Minimo(int a, int b);
+int Levenshtein(char *s1,char *s2);
 
 #endif // OPERACIONESDEUSUARIO_H_INCLUDED
